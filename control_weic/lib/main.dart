ee51a339afc4fe625fe3091396537f630cf79391
MarceloJSCesar <jsdevmarcelo@gmail.com>
2021-10-16 17:17:12 -0400

    project started from scratch(control_weic), main file created

---
+++ control_weic/lib/main.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/widgets.dart';
+import 'src/control_weic.dart';
+
+void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  runApp(const ControlWeic());
+}
---
+++ control_weic/lib/src/control_weic.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class ControlWeic extends StatelessWidget {
+  const ControlWeic({Key? key}) : super(key: key);
+
+  @override
+  MaterialApp build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Container(),
+    );
+  }
+}
