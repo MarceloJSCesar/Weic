2dd5388dced39a4aa4d7c730eb65b93b352482fe
MarceloJSCesar <jsdevmarcelo@gmail.com>
2021-10-16 17:02:42 -0400

    project started from scratch, main file created

--- weic/lib/core/config/app_colors.dart
+++
@@ -1,5 +0,0 @@
-import 'package:flutter/material.dart';
-
-class AppColors {
-  static final Color mainPrefixColor = Color(0xFF00ced1);
-}
--- weic/lib/main.dart
+++ weic/lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/widgets.dart';
-import 'core/weic.dart';
+import 'src/weic.dart';
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
-  runApp(Weic());
+  runApp(const Weic());
 }
