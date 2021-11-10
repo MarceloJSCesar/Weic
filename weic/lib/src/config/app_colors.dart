56834a3d30f911bd947aa5402d6b88366180968c
MarceloJSCesar <jsdevmarcelo@gmail.com>
2021-11-10 04:34:43 -0500

    config files created

---
+++ weic/lib/src/config/app_colors.dart
@@ -0,0 +1,5 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static final Color mainPrefixColor = Color(0xFF00ced1);
+}
--- weic/lib/src/views/authentication/login/login_view.dart
+++ weic/lib/src/views/authentication/login/login_view.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:weic/src/config/app_colors.dart';
+import 'package:weic/src/config/app_decorations.dart';
+import 'package:weic/src/config/app_img_path.dart';
+import 'package:weic/src/config/app_textstyles.dart';
 
 class LoginView extends StatefulWidget {
   const LoginView({Key? key}) : super(key: key);
@@ -8,8 +12,98 @@ class LoginView extends StatefulWidget {
 }
 
 class _LoginViewState extends State<LoginView> {
+  final _formkey = GlobalKey<FormState>();
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: Container(
+        decoration: AppDecorations.mainDecoration,
+        alignment: Alignment.topCenter,
+        child: SingleChildScrollView(
+          child: Form(
+            key: _formkey,
+            child: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 20),
+              child: Column(
+                children: <Widget>[
+                  ClipOval(
+                    child: Image(
+                      width: 300,
+                      image: AssetImage(AppImgPath.logoImageUrl),
+                    ),
+                  ),
+                  Card(
+                    elevation: 10.0,
+                    shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(20)),
+                    color: Colors.black,
+                    shadowColor: Colors.black,
+                    child: Container(
+                      margin: const EdgeInsets.symmetric(
+                        horizontal: 20,
+                        vertical: 40,
+                      ),
+                      child: Column(
+                        children: <Widget>[
+                          TextFormFieldComponent(
+                            hintText: 'Email',
+                            saveValue: (val) => email = val,
+                            isPasswordField: false,
+                            isEmailField: true,
+                            validateField: (val) => _auth.validateEmail(val),
+                          ),
+                          Divider(),
+                          TextFormFieldComponent(
+                            hintText: 'Password',
+                            saveValue: (val) => password = val,
+                            isPasswordField: true,
+                            isEmailField: false,
+                            validateField: (val) => _auth.validatePassword(val),
+                            showPassword: () => setState(() {
+                              _auth.viewPasswordValue();
+                            }),
+                            obscureText: () {
+                              if (_auth.viewPassword == true) {
+                                return true;
+                              } else {
+                                return false;
+                              }
+                            },
+                            viewPassword: _auth.viewPassword,
+                          ),
+                          SizedBox(
+                            height: 40,
+                          ),
+                          ElevatedButton(
+                            style: ButtonStyle(
+                              backgroundColor: MaterialStateProperty.all<Color>(
+                                AppColors.mainPrefixColor,
+                              ),
+                            ),
+                            onPressed: () {},
+                            child: Text('Entrar'),
+                          ),
+                          SizedBox(
+                            height: 10,
+                          ),
+                          GestureDetector(
+                            child: Text(
+                              'Nao tenho uma conta, Registrar',
+                              style: AppTextStyles.hintTextStyle,
+                            ),
+                            onTap: () => Navigator.of(context)
+                                .pushReplacementNamed('/register'),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
   }
 }
