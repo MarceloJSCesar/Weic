ef0e3fb949fb5ac6c1a6dc1b3f12eea57c1b4b53
MarceloJSCesar <jsdevmarcelo@gmail.com>
2021-12-09 13:22:50 -0800

    firebase configured

--- weic/lib/src/views/login/login_view.dart
+++ weic/lib/src/views/login/login_view.dart
@@ -6,6 +6,7 @@ import '../../config/app_assetsnames.dart';
 import '../../components/login/body/login_body.dart';
 import '../../controllers/login/login_controller.dart';
 import '../../components/login/textfield/app_text_field_component.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
 class LoginView extends StatefulWidget {
   LoginView({Key? key}) : super(key: key);
@@ -17,6 +18,23 @@ class _LoginViewState extends State<LoginView> {
   final _formKey = GlobalKey<FormState>();
   String? email;
   String? password;
+
+  final _collection = FirebaseFirestore.instance.collection('user');
+
+  void storedata() async {
+    await _collection
+        .add({'name': 'Marcelo', 'idade': '19 anos'})
+        .then((value) => print('data stored $value'))
+        .catchError((value) => print('error occured $value'));
+  }
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    storedata();
+  }
+
   @override
   Scaffold build(BuildContext context) {
     return Scaffold(
@@ -41,11 +59,14 @@ class _LoginViewState extends State<LoginView> {
                     Expanded(
                       child: Container(),
                     ),
-                    Container(
-                      margin: const EdgeInsets.only(bottom: 10),
-                      child: Text(
-                        'Developed by Marcelo Cesar',
-                        style: AppTextStyles.blackTextStyle,
+                    GestureDetector(
+                      onTap: () async => storedata(),
+                      child: Container(
+                        margin: const EdgeInsets.only(bottom: 10),
+                        child: Text(
+                          'Developed by Marcelo Cesar',
+                          style: AppTextStyles.blackTextStyle,
+                        ),
                       ),
                     ),
                   ],
