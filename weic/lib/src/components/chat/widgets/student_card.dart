dabe62280192c152c19fd03f801ee068ed3c69bb
MarceloJSCesar <jsdevmarcelo@gmail.com>
2022-01-11 14:20:25 -0800

    my profile not appearing in studantes

--- weic/lib/src/components/chat/widgets/student_card.dart
+++ weic/lib/src/components/chat/widgets/student_card.dart
@@ -15,79 +15,82 @@ class StudentCard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      margin: const EdgeInsets.symmetric(vertical: 8),
-      child: ListTile(
-        leading: CircleAvatar(
-          radius: 30,
-          backgroundImage: NetworkImage(anotherStudent.profilePhoto as String),
-        ),
-        title: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: <Widget>[
-            Text(anotherStudent.name as String),
-            Row(
-              children: <Widget>[
-                Expanded(
-                  child: Container(
-                    height: 30,
-                    alignment: Alignment.center,
-                    padding: const EdgeInsets.all(6),
-                    decoration: BoxDecoration(
-                      color: Colors.blue,
-                      borderRadius: BorderRadius.circular(8),
-                    ),
-                    child: GestureDetector(
-                      onTap: () => Navigator.of(context).push(
-                        MaterialPageRoute(
-                          builder: (_) => MensagesScreen(
-                            myId: myId,
-                            anotherStudent: anotherStudent,
+    return anotherStudent.id != myId
+        ? Container(
+            margin: const EdgeInsets.symmetric(vertical: 8),
+            child: ListTile(
+              leading: CircleAvatar(
+                radius: 30,
+                backgroundImage:
+                    NetworkImage(anotherStudent.profilePhoto as String),
+              ),
+              title: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  Text(anotherStudent.name as String),
+                  Row(
+                    children: <Widget>[
+                      Expanded(
+                        child: Container(
+                          height: 30,
+                          alignment: Alignment.center,
+                          padding: const EdgeInsets.all(6),
+                          decoration: BoxDecoration(
+                            color: Colors.blue,
+                            borderRadius: BorderRadius.circular(8),
                           ),
-                        ),
-                      ),
-                      child: FittedBox(
-                        child: Text(
-                          'Enviar Mensagem',
-                          style: AppTextStyles.studentCardButtonTextStyle,
-                        ),
-                      ),
-                    ),
-                  ),
-                ),
-                Expanded(
-                  child: Container(
-                    height: 30,
-                    alignment: Alignment.center,
-                    padding: const EdgeInsets.all(4),
-                    margin: const EdgeInsets.only(left: 4),
-                    decoration: BoxDecoration(
-                      color: Colors.blue,
-                      borderRadius: BorderRadius.circular(8),
-                    ),
-                    child: GestureDetector(
-                      onTap: () {
-                        Navigator.of(context).push(
-                          MaterialPageRoute(
-                            builder: (_) => ProfileViewer(
-                              myID: myId,
-                              studentID: anotherStudent.id as String,
+                          child: GestureDetector(
+                            onTap: () => Navigator.of(context).push(
+                              MaterialPageRoute(
+                                builder: (_) => MensagesScreen(
+                                  myId: myId,
+                                  anotherStudent: anotherStudent,
+                                ),
+                              ),
+                            ),
+                            child: FittedBox(
+                              child: Text(
+                                'Enviar Mensagem',
+                                style: AppTextStyles.studentCardButtonTextStyle,
+                              ),
                             ),
                           ),
-                        );
-                      },
-                      child: Text(
-                        'Ver Perfil',
-                        style: AppTextStyles.studentCardButtonTextStyle,
+                        ),
                       ),
-                    ),
+                      Expanded(
+                        child: Container(
+                          height: 30,
+                          alignment: Alignment.center,
+                          padding: const EdgeInsets.all(4),
+                          margin: const EdgeInsets.only(left: 4),
+                          decoration: BoxDecoration(
+                            color: Colors.blue,
+                            borderRadius: BorderRadius.circular(8),
+                          ),
+                          child: GestureDetector(
+                            onTap: () {
+                              Navigator.of(context).push(
+                                MaterialPageRoute(
+                                  builder: (_) => ProfileViewer(
+                                    myID: myId,
+                                    studentID: anotherStudent.id as String,
+                                  ),
+                                ),
+                              );
+                            },
+                            child: Text(
+                              'Ver Perfil',
+                              style: AppTextStyles.studentCardButtonTextStyle,
+                            ),
+                          ),
+                        ),
+                      ),
+                    ],
                   ),
-                ),
-              ],
+                ],
+              ),
             ),
-          ],
-        ),
-      ),
-    );
+          )
+        : Container();
   }
 }
