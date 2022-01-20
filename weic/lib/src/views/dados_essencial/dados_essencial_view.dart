03c9c9d636da78b8cd26fe85f86950ef4a5730f8
MarceloJSCesar <jsdevmarcelo@gmail.com>
2022-01-20 08:03:19 -0800

    essectial data component file created, and that file removed from home compoenents

--- weic/lib/src/views/dados_essencial/dados_essencial_view.dart
+++ weic/lib/src/views/dados_essencial/dados_essencial_view.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:weic/src/components/home/widgets/insert_essencial_data_view.dart';
-import 'package:weic/src/models/student.dart';
+import '../../models/student.dart';
+import '../../components/insert_essential_data/widgets/insert_essencial_data_view.dart';
 
 class DadosEssenciaisView extends StatefulWidget {
   final Student student;
