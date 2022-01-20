9ea1ddef10944eaf27c522f7681b067ffb741231
MarceloJSCesar <jsdevmarcelo@gmail.com>
2022-01-20 11:02:31 -0800

    search_student_ view created

---
+++ weic/lib/src/views/search_student/search_student_view.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class SearchStudentView extends StatelessWidget {
+  const SearchStudentView({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
