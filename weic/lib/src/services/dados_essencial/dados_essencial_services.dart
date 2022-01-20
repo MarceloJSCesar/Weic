3387f364e71b0c5304b68913733317975e9c6f73
MarceloJSCesar <jsdevmarcelo@gmail.com>
2022-01-20 12:51:51 -0800

    updatePassword fuction created in dados essenciais and added to view

---
+++ weic/lib/src/services/dados_essencial/dados_essencial_services.dart
@@ -0,0 +1,50 @@
+import 'dart:io';
+
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:image_picker/image_picker.dart';
+import 'package:firebase_storage/firebase_storage.dart';
+import 'package:weic/src/models/student.dart';
+
+class DadosEssenciaisServices {
+  final _authInstance = FirebaseAuth.instance;
+  final ImagePicker _imagePicker = ImagePicker();
+  Future<XFile?> takePhotoFromCamera() async {
+    XFile? imageSource = await _imagePicker.pickImage(
+      source: ImageSource.camera,
+      maxHeight: 300,
+      maxWidth: 300,
+    );
+    return imageSource;
+  }
+
+  Future<XFile?> pickPhotoFromGalery() async {
+    XFile? imageSource = await _imagePicker.pickImage(
+      source: ImageSource.gallery,
+      maxHeight: 300,
+      maxWidth: 300,
+    );
+    return imageSource;
+  }
+
+  Future<String> uploadPhoto({
+    required Student student,
+    required File file,
+  }) async {
+    final _storageRef = FirebaseStorage.instance.ref();
+    final _uploadTask = await _storageRef
+        .child('studentsProfilePhotos')
+        .child('student ${student.id}')
+        .putFile(file);
+    final _photoUrl = await _uploadTask.ref.getDownloadURL();
+    return _photoUrl;
+  }
+
+  Future<bool?> updatePassword({required String newPassword}) async {
+    await _authInstance.currentUser?.updatePassword(newPassword).then((_) {
+      return true;
+    }).catchError((errorReturn) {
+      return false;
+    });
+  }
+}
