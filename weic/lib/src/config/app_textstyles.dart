74d1db82c045e41d327db28321b527306bb67a84
MarceloJSCesar <jsdevmarcelo@gmail.com>
2022-03-04 22:07:01 -0100

    login snack bar set to dark

--- weic/lib/src/config/app_textstyles.dart
+++ weic/lib/src/config/app_textstyles.dart
@@ -93,7 +93,7 @@ class AppTextStyles {
   //  profile
   final TextStyle snackbarTextButtonTextStyle = GoogleFonts.notoSans(
     fontSize: 16,
-    color: Colors.white,
+    color: Colors.black,
     fontWeight: FontWeight.normal,
   );
   // student search
